df6572ec2562c4cf1db82c01073e7b5729f94a2f
Alphonse-xu <bluerailgun@outlook.com>
2020-02-11 22:33:21 +0000

    little

--- RoguelikeGame/Assets/Scripts/RoomGenerator.cs
+++ RoguelikeGame/Assets/Scripts/RoomGenerator.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class RoomGenerator : MonoBehaviour
 {
+    [Header("房间信息")]
+    public GameObject roomPrefab;
+    public int roomNumber;
+    public Color startcolor, endcolor;
+
+    [Header("位置控制")]
+    public Transform generatorPoint;
+    public float xoffset;
+    public float yoffset;
+
+
     // Start is called before the first frame update
     void Start()
     {
