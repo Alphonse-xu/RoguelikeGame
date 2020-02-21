1594b20efe11a7f9e653a4b1fe58e16eefa97fcc
Alphonse-xu <b8067791@newcastle.ac.uk>
2020-02-21 16:31:39 +0000

    map

--- RoguelikeGame/Assets/Scripts/Room.cs
+++ RoguelikeGame/Assets/Scripts/Room.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Room : MonoBehaviour
 {
     public GameObject doorLeft, doorRight, doorUp, doorDown;
+
     public bool roomLeft, roomRight, roomUp, roomDown;//判断上下左右是否有房间
+
+    public Text text;
+    public int stepToStart;//距离初始点的网格距离
+
+    public int doorNumber;//当前房间的门的数量/入口数量
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +22,25 @@ public class Room : MonoBehaviour
         doorUp.SetActive(roomUp);
         doorDown.SetActive(roomDown);
     }
+
+    public void UpdateRoom()
+    {
+        //计算距离初始点的网格距离
+        stepToStart = (int)(Mathf.Abs(transform.position.x / 18) + Mathf.Abs(transform.position.y / 9));
+
+        text.text = stepToStart.ToString();
+
+        if (roomUp)
+            doorNumber++;
+        if (roomDown)
+            doorNumber++;
+        if (roomLeft)
+            doorNumber++;
+        if (roomRight)
+            doorNumber++;
+    }
+
+
 
     // Update is called once per frame
     void Update()
--- RoguelikeGame/Assets/Scripts/RoomGenerator.cs
+++ RoguelikeGame/Assets/Scripts/RoomGenerator.cs
@@ -24,7 +24,6 @@ public class RoomGenerator : MonoBehaviour
     public List<Vector2> roomNotVisited = new List<Vector2>();     //存储玩家没有去过的房间
 
 
-
     public GameObject roomPrefab;
     public int roomNumber;
     public Color startColor, endColor;
@@ -35,9 +34,17 @@ public class RoomGenerator : MonoBehaviour
     public float xOffset;
     public float yOffset;
     public LayerMask roomLayer;
-
+    public int maxStep;
 
     public List<Room> rooms = new List<Room> ();
+
+    List<GameObject> farRooms = new List<GameObject>();
+
+    List<GameObject> lessFarRooms = new List<GameObject>();
+
+    List<GameObject> oneWayRooms = new List<GameObject>();
+
+    public WallType wallType;
 
     // Start is called before the first frame update
     void Start()
@@ -61,9 +68,11 @@ public class RoomGenerator : MonoBehaviour
             //    endRoom = room.gameObject;
             //}
             SetupRoom(room, room.transform.position);
-
-
         }
+
+        //找到最后房间
+        FindEndRoom();
+
         endRoom.GetComponent<SpriteRenderer>().color = endColor;//改变房间的颜色
 
     }
@@ -109,6 +118,90 @@ public class RoomGenerator : MonoBehaviour
         newRoom.roomDown = Physics2D.OverlapCircle(roomPosition + new Vector3(0, -yOffset, 0), 0.2f, roomLayer);
         newRoom.roomLeft = Physics2D.OverlapCircle(roomPosition + new Vector3(-xOffset, 0, 0), 0.2f, roomLayer);
         newRoom.roomRight = Physics2D.OverlapCircle(roomPosition + new Vector3(xOffset, 0, 0), 0.2f, roomLayer);
+
+        newRoom.UpdateRoom();//参考上面Room脚本的内容
+        
+        switch (newRoom.doorNumber)
+        {
+            case 1:
+                if (newRoom.roomUp)
+                    Instantiate(wallType.singleUp, roomPosition, Quaternion.identity);
+                if (newRoom.roomDown)
+                    Instantiate(wallType.singleBottom, roomPosition, Quaternion.identity);
+                if (newRoom.roomLeft)
+                    Instantiate(wallType.singleLeft, roomPosition, Quaternion.identity);
+                if (newRoom.roomRight)
+                    Instantiate(wallType.singleRight, roomPosition, Quaternion.identity);
+                break;
+            case 2:
+                if (newRoom.roomLeft && newRoom.roomUp)
+                    Instantiate(wallType.doubleLU, roomPosition, Quaternion.identity);
+                if (newRoom.roomLeft && newRoom.roomRight)
+                    Instantiate(wallType.doubleLR, roomPosition, Quaternion.identity);
+                if (newRoom.roomLeft && newRoom.roomDown)
+                    Instantiate(wallType.doubleLB, roomPosition, Quaternion.identity);
+                if (newRoom.roomUp && newRoom.roomRight)
+                    Instantiate(wallType.doubleUR, roomPosition, Quaternion.identity);
+                if (newRoom.roomUp && newRoom.roomDown)
+                    Instantiate(wallType.doubleUB, roomPosition, Quaternion.identity);
+                if (newRoom.roomRight && newRoom.roomDown)
+                    Instantiate(wallType.doubleRB, roomPosition, Quaternion.identity);
+                break;
+            case 3:
+                if (newRoom.roomLeft && newRoom.roomUp && newRoom.roomRight)
+                    Instantiate(wallType.tripleLUR, roomPosition, Quaternion.identity);
+                if (newRoom.roomLeft && newRoom.roomRight && newRoom.roomDown)
+                    Instantiate(wallType.tripleLRB, roomPosition, Quaternion.identity);
+                if (newRoom.roomDown && newRoom.roomUp && newRoom.roomRight)
+                    Instantiate(wallType.tripleURB, roomPosition, Quaternion.identity);
+                if (newRoom.roomLeft && newRoom.roomUp && newRoom.roomDown)
+                    Instantiate(wallType.tripleLUB, roomPosition, Quaternion.identity);
+                break;
+            case 4:
+                if (newRoom.roomLeft && newRoom.roomUp && newRoom.roomRight && newRoom.roomDown)
+                    Instantiate(wallType.fourDoors, roomPosition, Quaternion.identity);
+                break;
+        }
+    }
+
+    public void FindEndRoom()
+    {
+        //最大数值 最远距离数字
+        for (int i = 0; i < rooms.Count; i++)
+        {
+            if (rooms[i].stepToStart > maxStep)
+                maxStep = rooms[i].stepToStart;
+        }
+
+        //获得最远房间和第二远
+        foreach (var room in rooms)
+        {
+            if (room.stepToStart == maxStep)
+                farRooms.Add(room.gameObject);
+            if (room.stepToStart == maxStep - 1)
+                lessFarRooms.Add(room.gameObject);
+        }
+
+        for (int i = 0; i < farRooms.Count; i++)
+        {
+            if (farRooms[i].GetComponent<Room>().doorNumber == 1)
+                oneWayRooms.Add(farRooms[i]);//最远房间里的单侧门加入
+        }
+
+        for (int i = 0; i < lessFarRooms.Count; i++)
+        {
+            if (lessFarRooms[i].GetComponent<Room>().doorNumber == 1)
+                oneWayRooms.Add(lessFarRooms[i]);//第二远远房间里的单侧门加入
+        }
+
+        if (oneWayRooms.Count != 0)
+        {
+            endRoom = oneWayRooms[Random.Range(0, oneWayRooms.Count)];
+        }
+        else
+        {
+            endRoom = farRooms[Random.Range(0, farRooms.Count)];
+        }
     }
 
     /// <summary>
@@ -127,4 +220,14 @@ public class RoomGenerator : MonoBehaviour
 
     }
 
+}
+
+
+[System.Serializable]
+public class WallType
+{
+    public GameObject singleLeft, singleRight, singleUp, singleBottom,
+                  doubleLU, doubleLR, doubleLB, doubleUR, doubleUB, doubleRB,
+                  tripleLUR, tripleLUB, tripleURB, tripleLRB,
+                  fourDoors;
 }
