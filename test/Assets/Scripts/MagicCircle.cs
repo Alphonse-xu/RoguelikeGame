014ef097e85572d35472ffe3f73cd9e3c747a23b
Alphonse-xu <bluerailgun@outlook.com>
2020-08-02 18:27:53 +0100

    error

---
+++ test/Assets/Scripts/MagicCircle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicCircle : MonoBehaviour
+{ 
+    public GameObject wall;
+    public ParticleSystem ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F)){
+ 
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(ps != null){
+            ps.Play();
+            Destroy(ps.transform.parent.gameObject, 1.0F);
+        }
+            
+    }
+    private void OnTriggerStay(Collider other) {
+        wall.SendMessage("Lift");
+    }
+}
---
+++ test/Assets/Scripts/WallMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMove : MonoBehaviour
+{
+    public float maxY = 12.0F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.E)){
+            Lift();
+        }
+        
+    }
+
+    public void Echo(){
+        print("noooooo");
+    }
+
+    public void Lift(){
+        if(transform.position.y <= 14.0F){
+            transform.position=new Vector3( transform.position.x,
+            transform.position.y + 0.1F, transform.position.z);
+        }
+    }
+}
